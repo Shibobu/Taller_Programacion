05a0fb9fb98302282431930ebc3f9a205d770c94
Shibobu <gonzalozyarad@gmail.com>
2023-05-04 17:02:26 -0300

    Uff descubrimietno

--- Trabajo Practico JS/Ejercicio1/Ej1_Script.js
+++ Trabajo Practico JS/Ejercicio1/Ej1_Script.js
@@ -1,9 +1,21 @@
-function ejercicio1(){
-    
-    document.getElementById("Text_containers").hidden = false
-    var n1 = Number(prompt("Ingrese el primer numero"));
-    var n2 = Number(prompt("Ingrese el segundo numero"))
 
+function ejercicio1(text){
+    show(text)
+    /*var n1 = Number(prompt("Ingrese el primer numero"));
+    var n2 = Number(prompt("Ingrese el segundo numero"))
+    */
+}
+
+function resolve(text){
+    var inputs = document.getElementById(text);
+    var inp = inputs.getElementsByTagName("input");
+    var n1 = Number(inp[0].value)
+    var n2 = Number(inp[1].value)
+    /*var values = [];
+    for(var i = 0; i < inp.length;i++){
+        values.push(Number(inp[i].value));
+    }*/
+    
     alert("Suma " + suma(n1,n2))
     alert("Resta: " + resta(n1,n2))
     alert("Multiplicacion: " + multp(n1,n2))
@@ -12,7 +24,17 @@ function ejercicio1(){
         alert("Division: " + res)
     }
     else alert("Todo mal capo, ingresaste un 0 no se puede dividir por eso ")
+
+    clear(text)
 }
+
+function clear(text){
+    var inputs = document.getElementById(text);
+    var inp = inputs.getElementsByTagName("input");
+    inp[0].value = ""
+    inp[1].value = ""
+}
+
 function suma(n1,n2){
     return n1 + n2
 }
@@ -30,4 +52,12 @@ function div(n1,n2){
         return NaN
     }
     return n1/n2
+}
+
+function hide(text){
+    document.getElementById(text).style.display = "none";
+}
+
+function show(text){
+    document.getElementById(text).style.display = "grid";
 }
